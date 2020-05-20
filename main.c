79f459a085fcf05c93466fc4daca148b770807f1
Adven97 <atomczak30@gmail.com>
2020-05-20 23:53:56 +0200

    few changes

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PRICE 9
 
 void show_states(int *state_list, int len){
-    int i;
-    int aa = sizeof(state_list);
-    printf("\n %d \n\n", aa);
-
     printf("LISTA STANOW \n");
+    int i;
     for(i=0; i < len; i++){
-        printf("stan q%d to %d \n", i+1, state_list[i]);
+        printf("stan %d to q%d \n", i+1, state_list[i]);
     }
 }
 
 int main()
 {
-    printf("BILETOMAT XD \n");
+    printf("BILETOMAT \n");
+    printf("Bilet na Basen koszyuje 9 zl \n");
     int state = 0;
-    int *state_list= malloc(sizeof(int));
     int counter = 0;
-
+    int *state_list= malloc(sizeof(int));
     for(;;)
     {
-        int input;
         printf("Prosze wprowadzic monete 1 2 lub 5zl \n");
+        int input;
+
         scanf("%d", &input);
 
         if(input == 1 || input == 2 || input == 5)
@@ -32,25 +31,28 @@ int main()
 
             counter++;
 
-            if(state == 9)
+            if(state == PRICE)
             {
-                printf("Bilet wydany :D \n");
+                printf("Bilet wydany \n");
                 show_states(state_list, counter);
                 return 1;
             }
-            if(state >= 10){
+            if(state > PRICE){
                 state = 10;
                 printf("Bilet nie zostaje wydany \n");
                 printf("Zwracam monety \n");
                 show_states(state_list, counter);
                 return -1;
             }
+
             printf("wrzucono %d zl \n", input);
             printf("aktualny stan to %d \n", state);
         }
-
-        //state_list = malloc(counter*sizeof(int));
-
+        else{
+            printf("Wprowadzono symbol spoza alfabetu \n");
+            state_list[counter] = 10;
+            counter++;
+        }
     }
     return 0;
 }
