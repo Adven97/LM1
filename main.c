03e75e4be26444ecdc248873ba65f66ebabbb1b6
Adven97 <atomczak30@gmail.com>
2020-05-21 11:05:02 +0200

    this shit doesnt work

--- main.c
+++ main.c
@@ -2,56 +2,68 @@
 #include <stdlib.h>
 #define PRICE 9
 
+/* Metoda pokazujaca wszystkie stany automatu. Liczba tych stanow jest rowna liczbie wrzuconych monet*/
 void show_states(int *state_list, int len){
     printf("LISTA STANOW \n");
     int i;
     for(i=0; i < len; i++){
-        printf("stan %d to q%d \n", i+1, state_list[i]);
+        printf("stan %d to %s \n", i+1, state_list[i]);
     }
 }
 
 int main()
 {
     printf("BILETOMAT \n");
-    printf("Bilet na Basen koszyuje 9 zl \n");
-    int state = 0;
+    printf("Bilet na Plywalnie kosztuje 9 zl \n");
+
+    /* 0 oznacza stan poczatkowy q0*/
+    char state[3] = "q0";
+    int sum = 0;
     int counter = 0;
-    int *state_list= malloc(sizeof(int));
+    char **state_list = NULL;
     for(;;)
     {
         printf("Prosze wprowadzic monete 1 2 lub 5zl \n");
-        int input;
 
+        int input;
         scanf("%d", &input);
 
+        /* Uzytkownik wrzuca monety 1 2 lub 5. Suma wrzuconych monet deteerminuje aktualny stan*/
         if(input == 1 || input == 2 || input == 5)
         {
-            state+= input;
-            state_list[counter] = state;
+            sum+= input;
+            //state = "q"+sum;
+            snprintf(state, 3, "q%d", sum); // puts string into buffer
+
+            //state_list[counter][4] = state;
+            state_list = (char**)realloc(state_list, (counter)*sizeof(*state_list));
+            state_list[counter] = (char*)malloc(sizeof(state));
+            strcpy(state_list[counter], state);
 
             counter++;
 
-            if(state == PRICE)
+            printf("wrzucono %d zl \n", input);
+            printf("aktualny stan to %s \n", state);
+            printf("W sumie wrzucono %d zl \n", sum);
+
+            if(sum == PRICE)
             {
-                printf("Bilet wydany \n");
+                printf("Bilet zostaje wydany \n");
                 show_states(state_list, counter);
                 return 1;
             }
-            if(state > PRICE){
-                state = 10;
+            if(sum > PRICE){
+                snprintf(state, 3, "q%d", 10); // puts string into buffer
                 printf("Bilet nie zostaje wydany \n");
                 printf("Zwracam monety \n");
                 show_states(state_list, counter);
                 return -1;
             }
 
-            printf("wrzucono %d zl \n", input);
-            printf("aktualny stan to %d \n", state);
+
         }
         else{
             printf("Wprowadzono symbol spoza alfabetu \n");
-            state_list[counter] = 10;
-            counter++;
         }
     }
     return 0;
